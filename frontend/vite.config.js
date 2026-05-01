f7365e5d5b58a59c52727d7b1b9047143bf637fd
RAGHURAJ09 <sincereraghurajrajpoot2819@gmail.com>
2026-05-01 08:04:20 +0530

    Update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
-const { defineConfig } = require('vite')
-const react = require('@vitejs/plugin-react')
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     host: true
+  },
+  build: {
+    outDir: 'dist'
+  },
+  define: {
+    'process.env': {}
   }
 })
